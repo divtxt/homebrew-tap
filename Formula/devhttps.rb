d63be23c97e50763d0154aaf720d5b3601d72aff
Div Shekhar <1030697+divtxt@users.noreply.github.com>
2026-03-08 18:43:50 +0530

    devhttps 1.0.1

--- Formula/devhttps.rb
+++ Formula/devhttps.rb
@@ -1,25 +1,25 @@
 class Devhttps < Formula
   desc "Automatic https reverse proxy for local development"
   homepage "https://github.com/divtxt/devhttps"
-  version "1.0.0"
+  version "1.0.1"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/divtxt/devhttps/releases/download/v1.0.0/devhttps_darwin_arm64.tar.gz"
-      sha256 "647332d03f43cfb089f4894af9fa9700c560a049af3357de368612e27f8629de"
+      url "https://github.com/divtxt/devhttps/releases/download/v1.0.1/devhttps_darwin_arm64.tar.gz"
+      sha256 "311b9517ea5788e3cc945832a4bfff6486501e984f82e19311972e6246abad8d"
     else
-      url "https://github.com/divtxt/devhttps/releases/download/v1.0.0/devhttps_darwin_amd64.tar.gz"
-      sha256 "fef70157bec9a183c680fb8b358877846278349f469ea603aba9f99e735989ec"
+      url "https://github.com/divtxt/devhttps/releases/download/v1.0.1/devhttps_darwin_amd64.tar.gz"
+      sha256 "59010f1f8e28693bfd8c05b1562dc0c0526f3f2dae34ac7635ff5b964ff5112b"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/divtxt/devhttps/releases/download/v1.0.0/devhttps_linux_arm64.tar.gz"
-      sha256 "a93e21c42e641059dab504c932b5a1a8cb07e7037c3a73116b6eeecb0ca869d8"
+      url "https://github.com/divtxt/devhttps/releases/download/v1.0.1/devhttps_linux_arm64.tar.gz"
+      sha256 "afc575f8df6764ab6917927ac852d548b9f25ed1968d2318fe12d41005f1f9b4"
     else
-      url "https://github.com/divtxt/devhttps/releases/download/v1.0.0/devhttps_linux_amd64.tar.gz"
-      sha256 "dc305525148224bb4ca5f3741fa673038c9f525db17fab6aac900c3a31b4509e"
+      url "https://github.com/divtxt/devhttps/releases/download/v1.0.1/devhttps_linux_amd64.tar.gz"
+      sha256 "03e6d960f5a49da45d59fe31e6fb144fd88e0c2406c96e18fd76df3548ba7281"
     end
   end
 
